fc2e628aaf6d5e9004c67b4bf9ca5ae8e3779447
Petyr B #2 <103116958+PetyrB2@users.noreply.github.com>
2022-07-02 18:28:29 +0100

    Home.js
    Needs some gfx and what to link 
    But has a few ideas for us to work on

---
+++ Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="text-center">
+      <h1>Welcome to Q A CINEMAS !</h1>
+
+
+          <h3>Buy YOUR tickets NOW !</h3>
+
+          <h2>Latest Movies(image)</h2>
+          <h2>Our New Deluxe Screen!(image)</h2>
+          <h2>Family & Members Deals (coming soon)</h2>
+    </div>
+  )
+}
+
+export default Home;
