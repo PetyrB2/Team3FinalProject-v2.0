9ada6003085b1ffad91bf2ae90e87692b81d61f3
Petyr B #2 <103116958+PetyrB2@users.noreply.github.com>
2022-07-02 18:23:52 +0100

    About.js
    Basic stuff - will probably be the last for us to fill in

---
+++ About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="text-center">
+      
+      About QA Cinema 
+      
+      <p> & </p>
+        <p>The Awesome Team that made this website</p>
+      </div>
+  )
+}
+
+export default About
