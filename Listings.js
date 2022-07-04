279e3a57da7b3f8e620c5e4ad51c682d7fc8072d
Petyr B #2 <103116958+PetyrB2@users.noreply.github.com>
2022-07-04 11:14:14 +0100

    Listings.js Better
    A btter and working version of Listings.js
    Needs tidying up of the image sizer and the text.

--- Listings.js
+++ Listings.js
@@ -1,125 +1,115 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "../assets/css/searchbar.css";
+
+// import "../assets/css/searchbar.css";
 import { Link } from "react-router-dom";
 // import StripeCheckout from "react-stripe-checkout";
 
 function Listings() {
+  const movie = "avengers";
 
-  const [titleState, setTitleState] = useState("Enter a movie title below");
   const [data, setData] = useState("");
-  const fetchMovie = async () => {
 
+  const movieB = "click";
+
+  const [dataB, setDataB] = useState("");
+
+  const movieC = "Memento";
+
+  const [dataC, setDataC] = useState("");
+
+  const movieD = "The%20Boys";
+
+  const [dataD, setDataD] = useState("");
+
+  const fetchMovie = async (movieName, setInfo) => {
     const { data } = await axios.get(
-
-      //api key here -***682f64da
-
-      "http://www.omdbapi.com/?i=tt3896198&apikey=682f64da&t=Avengers"
-
+      "http://www.omdbapi.com/?i=tt3896198&apikey=682f64da&t=" + movieName
     );
 
-    setData(data);
-
-
-
+    setInfo(data);
   };
 
   useEffect(() => {
-    fetchMovie();
+    fetchMovie(movie, setData);
 
-  }, [])
+    fetchMovie(movieB, setDataB);
+
+    fetchMovie(movieC, setDataC);
+
+    fetchMovie(movieD, setDataD);
+  }, []);
 
   return (
-
     <div className="Listings">
+      <p id="Listings"></p>
+      <div className="row">
+        {/* Film A */}
+        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 border shadow gt-2 text-center">
+          <h2 className="mb-4">{data.Title}</h2>
+          <h5>Released: {data.Released}</h5>
 
+          <Link to="/Bookings">
+            <img src={data.Poster} alt="The poster" width="40%"></img>
+          </Link>
+          <h4>{data.Genre}</h4>
 
-      {/* <h2>{data.Title}</h2>
-             */}
-      {/* <StripeCheckout stripeKey="" token="" name="Buy Your Ticket">
-            <button className="btn-large blue"></button>
-            </StripeCheckout> */}
+          <h5>{data.Plot}</h5>
 
-      {/* <img src={data.Poster} alt="The poster" width='20%'></img>
+          <h4>Director: {data.Director}</h4>
+        </div>
 
-            <h5>Release date: {data.Released}</h5>
+        {/* Film B */}
+        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 border shadow gt-2 text-center">
+          <h2>{dataB.Title} </h2>
+          <h5>Released: {dataB.Released}</h5>
 
-            <h4>Director: {data.Director}</h4> */}
+          <Link to="/Bookings">
+            <button className="btn btn-outline-secondary">
+              <img src={dataB.Poster} alt="The poster" width="40%" />
+            </button>
+          </Link>
+          <h6>{dataB.Genre}</h6>
+          <h6>{dataB.Plot}</h6>
+          <h6>Director: {dataB.Director}</h6>
+        </div>
 
-      {/* <h4>{data.Genre}</h4>
+        {/* Film C */}
+        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 border shadow gt-2 text-center">
+          <h2>{dataC.Title} </h2>
+          <h5>Released: {dataC.Released}</h5>
 
-            <h5>{data.Plot}</h5>
+          <Link to="/Bookings">
+            <button className="btn btn-outline-secondary">
+              <img src={dataC.Poster} alt="The poster"></img>
+            </button>
+          </Link>
+          <h4>{dataC.Genre}</h4>
 
-            <h5>Rated: {data.Rated}</h5>
-            <h6>IMdb: {data.imdbRating}/10</h6> */}
+          <h5>{dataC.Plot}</h5>
 
-      <table>
-        <colgroup>
-          <col span="2" />
-          <col />
-        </colgroup>
-        <tr>
-          <th> <h2>{data.Title}</h2></th>
-          <th> <h2>{data.Title}</h2></th>
-          <th> <h2>{data.Title}</h2></th>
-          <th> <h2>{data.Title}</h2></th>
-        </tr>
-        <tr >
-          <td>
-            <Link to="/Bookings"><button type="button" className="btn btn-outline-secondary">
-              <img src={data.Poster} alt="The poster"></img></button>
-            </Link>
-          </td>
-          <td>
-            <Link to="/Bookings"><button type="button" className="btn btn-outline-secondary">
-              <img src={data.Poster} alt="The poster"></img></button>
-            </Link>
-          </td>
-          <td>
-            <Link to="/Bookings"><button type="button" className="btn btn-outline-secondary">
-              <img src={data.Poster} alt="The poster"></img></button>
-            </Link>
-          </td>
-          <td>
-            <Link to="/Bookings"><button type="button" className="btn btn-outline-secondary">
-              <img src={data.Poster} alt="The poster"></img></button>
-            </Link>
-          </td>
-        </tr>
-        <tr>
-          <td><h5>Released: {data.Released}</h5></td>
-          <td><h5>Released: {data.Released}</h5></td>
-          <td><h5>Released: {data.Released}</h5></td>
-          <td><h5>Released: {data.Released}</h5></td>
-        </tr>
-        <tr>
-          <td> <h4>{data.Genre}</h4> </td>
-          <td> <h4>{data.Genre}</h4> </td>
-          <td> <h4>{data.Genre}</h4> </td>
-          <td> <h4>{data.Genre}</h4> </td>
-        </tr>
-        <tr>
-          <td><h5>{data.Plot}</h5></td>
-          <td><h5>{data.Plot}</h5></td>
-          <td><h5>{data.Plot}</h5></td>
-          <td><h5>{data.Plot}</h5></td>
-        </tr>
-        <tr>
-          <td> <h4>Director: {data.Director}</h4> </td>
-          <td>  <h4>Director: {data.Director}</h4> </td>
-          <td>  <h4>Director: {data.Director}</h4> </td>
-          <td>  <h4>Director: {data.Director}</h4> </td>
-        </tr>
+          <h4>Director: {dataC.Director}</h4>
+        </div>
 
-      </table>
+        {/* Film D */}
+        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 border shadow gt-2 text-center">
+          <h2>{dataD.Title} </h2>
+          <h5>Released: {dataD.Released}</h5>
 
+          <Link to="/Bookings">
+            <button className="btn btn-outline-secondary">
+              <img src={dataD.Poster} alt="The poster"></img>
+            </button>
+          </Link>
+          <h6>{dataD.Genre}</h6>
 
+          <h6>{dataD.Plot}</h6>
+          <h6>Actors: {dataD.Actors}</h6>
+          <h6>Director: {dataD.Director}</h6>
+        </div>
+      </div>
     </div>
-
   );
-
 }
-
-
 
 export default Listings;
