5c6d865bcd1d96cc984e5f9ffae7e9bbb547a82a
Jordan <103116636+DundasBoots@users.noreply.github.com>
2022-07-05 11:38:45 +0100

    updated index to contain mongoose connect

--- index.js
+++ index.js
@@ -8,14 +8,33 @@ const messagesRoutes = require("./routes/Messages");
 const topicsRoutes = require("./routes/Topics");
 const checkoutRoutes = require("./routes/Stripe");
 
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb://localhost:27017/DiscussionBoard",
+  {
+    useNewUrlParser: true,
+  },
+  function (err) {
+    if (err) {
+      console.log("error db not connected");
+    } else {
+      console.log("db connected");
+    }
+  }
+);
+
 app.use("/comment", messagesRoutes);
 app.use("/topic", topicsRoutes);
 app.use("/checkout", checkoutRoutes);
 
-const server = app.listen(3001, () => {
+
+
+const server = app.listen(3000, () => {
   console.log(
     `Response server started successfully on port number ${
       server.address().port
     }`
   );
 });
+
+module.exports = server;
