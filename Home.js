83fbf684c17dd4c714214a6ae7108b77dea565ac
Petyr B #2 <103116958+PetyrB2@users.noreply.github.com>
2022-07-06 19:52:59 +0100

    Minor updates
    Picture added
    Should have more pics or buttons

--- Home.js
+++ Home.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="text-center">
-      <h1>Welcome to Q A CINEMAS !</h1>
+    <div className="text-center text-white">
+      <h1>Welcome to QA CINEMAS !</h1>
 
+      <h3>
+        <Link to="/Bookings">Buy YOUR tickets NOW !</Link>
+      </h3>
 
-          <h3>Buy YOUR tickets NOW !</h3>
+      <h2>
+        <Link to="/NewListings">Latest Movies(image)</Link>
+      </h2>
 
-          <h2>Latest Movies(image)</h2>
-          <h2>Our New Deluxe Screen!(image)</h2>
-          <h2>Family & Members Deals (coming soon)</h2>
+      <h2>
+        <Link to="/Screens">Our New Deluxe Screen!(image)</Link>
+      </h2>
+      <img
+        src={require("../assets/images/deluxe.png")}
+        alt="seats"
+        width="40%"
+      />
+
+      <h2>Family & Members Deals (coming soon)</h2>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
