fc492604f5ddcfff6461b30e1c310737bc7a14d6
5saad <66567037+5saad@users.noreply.github.com>
2022-07-06 11:44:05 +0100

    comment and replies added

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ app.use(express.json());
 const cors = require("cors");
 app.use(cors());
 
-const messagesRoutes = require("./routes/Messages");
+const commentRoutes = require("./routes/Comment");
+const replyRoutes = require("./routes/Reply");
 const topicsRoutes = require("./routes/Topics");
 const checkoutRoutes = require("./routes/Stripe");
 const saveCustomer = require("./routes/checkout");
@@ -24,7 +25,8 @@ mongoose.connect(
   }
 );
 
-app.use("/comment", messagesRoutes);
+app.use("/comment", commentRoutes);
+app.use("/reply", replyRoutes);
 app.use("/topic", topicsRoutes);
 app.use("/checkout", checkoutRoutes);
 app.use("/saveCustomer", saveCustomer);
