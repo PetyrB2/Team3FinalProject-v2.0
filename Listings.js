ca35ac6757c29d6d515c3814a13ebca3d6a40501
Petyr B #2 <103116958+PetyrB2@users.noreply.github.com>
2022-07-06 19:54:53 +0100

    Listings & Lightyear
    Listings 4 in a row
    may split into 2 x 2
    Lightyear is a demo
    What sort of data we can pull 
    How we can make it look etc

--- Listings.js
+++ Listings.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
-// import "../assets/css/searchbar.css";
 import { Link } from "react-router-dom";
-// import StripeCheckout from "react-stripe-checkout";
 
 function Listings() {
   const movie = "avengers";
@@ -14,11 +11,11 @@ function Listings() {
 
   const [dataB, setDataB] = useState("");
 
-  const movieC = "Memento";
+  const movieC = "Lightyear";
 
   const [dataC, setDataC] = useState("");
 
-  const movieD = "The%20Boys";
+  const movieD = "Rocky";
 
   const [dataD, setDataD] = useState("");
 
@@ -42,70 +39,84 @@ function Listings() {
 
   return (
     <div className="Listings">
-      <p id="Listings"></p>
-      <div className="row">
+      <div className="row text-white">
         {/* Film A */}
-        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 border shadow gt-2 text-center">
-          <h2 className="mb-4">{data.Title}</h2>
+        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 border shadow gt-2 text-center card w-25 bg-dark">
+          <h2>{data.Title}</h2>
           <h5>Released: {data.Released}</h5>
-
+          <h6>Duration: {data.Runtime}</h6>
           <Link to="/Bookings">
-            <img src={data.Poster} alt="The poster" width="40%"></img>
+            <button className="btn btn-outline-secondary">
+              <img src={data.Poster} alt="The poster"></img>
+            </button>
           </Link>
-          <h4>{data.Genre}</h4>
+          <div className="card-text">{data.Genre}</div>
 
-          <h5>{data.Plot}</h5>
+          <h6>{data.Plot}</h6>
 
           <h4>Director: {data.Director}</h4>
+          <Link to="/Bookings">
+            <button className="btn btn-outline-secondary">Book Now</button>
+          </Link>
         </div>
 
         {/* Film B */}
-        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 border shadow gt-2 text-center">
+        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 border shadow gt-2 text-center card w-25 card-title bg-dark">
           <h2>{dataB.Title} </h2>
           <h5>Released: {dataB.Released}</h5>
-
+          <h6>Duration: {dataB.Runtime}</h6>
           <Link to="/Bookings">
             <button className="btn btn-outline-secondary">
-              <img src={dataB.Poster} alt="The poster" width="40%" />
+              <img src={dataB.Poster} alt="The poster" />
             </button>
           </Link>
           <h6>{dataB.Genre}</h6>
           <h6>{dataB.Plot}</h6>
-          <h6>Director: {dataB.Director}</h6>
+          <h4>Director: {dataB.Director}</h4>
+          <Link to="/Bookings">
+            <button className="btn btn-outline-secondary">Book Now</button>
+          </Link>
         </div>
 
         {/* Film C */}
-        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 border shadow gt-2 text-center">
+        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 border shadow gt-2 text-center card w-25 bg-dark">
           <h2>{dataC.Title} </h2>
           <h5>Released: {dataC.Released}</h5>
-
-          <Link to="/Bookings">
+          <h6>Duration: {dataC.Runtime}</h6>
+          <Link to="/Lightyear">
             <button className="btn btn-outline-secondary">
               <img src={dataC.Poster} alt="The poster"></img>
             </button>
           </Link>
-          <h4>{dataC.Genre}</h4>
-
-          <h5>{dataC.Plot}</h5>
+          <h6>{dataC.Genre}</h6>
+          <h6>{dataC.Plot}</h6>
 
           <h4>Director: {dataC.Director}</h4>
+          <Link to="/Bookings">
+            <button className="btn btn-outline-secondary">Book Now</button>
+          </Link>
         </div>
 
         {/* Film D */}
-        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 border shadow gt-2 text-center">
-          <h2>{dataD.Title} </h2>
+        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 border shadow gt-2 text-center card w-25 bg-dark">
+          <h2>{dataD.Title}</h2>
           <h5>Released: {dataD.Released}</h5>
-
+          <h6>Duration: {dataD.Runtime}</h6>
           <Link to="/Bookings">
             <button className="btn btn-outline-secondary">
               <img src={dataD.Poster} alt="The poster"></img>
             </button>
           </Link>
+          <br />
+          <Link to="/Bookings">
+            <button className="btn btn-outline-secondary">Book Now</button>
+          </Link>
+          <br />
           <h6>{dataD.Genre}</h6>
 
           <h6>{dataD.Plot}</h6>
           <h6>Actors: {dataD.Actors}</h6>
-          <h6>Director: {dataD.Director}</h6>
+          <h4>Director: {dataD.Director}</h4>
         </div>
       </div>
     </div>
