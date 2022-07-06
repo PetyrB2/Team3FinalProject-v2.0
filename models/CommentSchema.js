49296303b6d5fd1fa060dd838748aeee5c204df9
5saad <66567037+5saad@users.noreply.github.com>
2022-07-06 11:43:21 +0100

    comment and replies made

---
+++ models/CommentSchema.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const Comment = mongoose.model(
+  "Comment",
+  new mongoose.Schema(
+    {
+      Username: {
+        type: String,
+        required: true,
+      },
+      Message: {
+        type: String,
+        required: true,
+      },
+
+      filmName: {
+        type: String,
+        required: false,
+      },
+      filmRating: {
+        type: Number,
+        required: false,
+        min: 1,
+        max: 10,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  )
+);
+
+module.exports = Comment;
