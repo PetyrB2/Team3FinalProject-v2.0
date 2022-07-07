5f01fc0f01677dfc8445a4f8ad591255b467e608
Jordan <103116636+DundasBoots@users.noreply.github.com>
2022-07-07 15:15:33 +0100

    added screen deluxe page

---
+++ Pages/ScreenDeluxe.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ScreenDeluxe = () => {
+  return (
+    <div>Screen Deluxe
+
+<noscript>You will need to enable Javascript to view this page</noscript>
+    </div>
+  )
+}
+
+export default ScreenDeluxe
