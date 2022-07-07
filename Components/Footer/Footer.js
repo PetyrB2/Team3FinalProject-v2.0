d325d7d8cc4e48ae076b393998405339a3b91e63
Jordan <103116636+DundasBoots@users.noreply.github.com>
2022-07-07 15:14:18 +0100

    added nav components

---
+++ Components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+//  import '../../assets/css/footer.css';
+import FooterCopyright from "./Footer-Components/FooterCopyright";
+
+function footer() {
+  return (
+    <div>
+      <FooterCopyright />
+    </div>
+  );
+}
+
+export default footer;
