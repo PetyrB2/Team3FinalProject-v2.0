0c1e3b591c5c39cfd7193e3d0ca3a203166d8fcc
Jordan <103116636+DundasBoots@users.noreply.github.com>
2022-07-08 10:25:27 +0100

    added board page

--- Pages/Board.js
+++ Pages/Board.js
@@ -1,56 +1,54 @@
-import React from "react";
-// import axios from "axios";
-
-// const [data, setData] = useState([]);
-
-// const fetchTopics = async () => {
-//   const res = await axios.get("http://localhost:3001/topic/read");
-//   setData(res.data);
-// };
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import TopicPost from "./../Components/Board/TopicPost";
 
 const Board = () => {
+  const [topicState, setTopicState] = useState("");
+  const [topicData, setTopicData] = useState([]);
+
+  const createTopic = () => {
+    axios.post("http://localhost:3001/topic/create", {
+      Topic: topicState,
+    });
+  };
+
+  //read all the topics
+
+  const fetchTopics = async () => {
+    const { data } = await axios.get("http://localhost:3001/topic/read");
+    setTopicData(data);
+  };
+
+  useEffect(() => {
+    fetchTopics();
+  }, [createTopic]);
+
   return (
-    <div className="text-center">
-      <h1>Welcome to the Board</h1>
-
-      <h3>
-        <h5>Topic: data something</h5>
-
-        <label className="form-label" id="topic-label">
-          New Topic:
-          <button>Reply</button>
-        </label>
-
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Please enter a new Topic..."
-        />
-
-        <input type="submit" value="Submit" />
-      </h3>
-
-      <div class="media">
-        {/* <img class="mr-3" src="..." alt="Generic placeholder image" /> */}
-        <div class="media-body">
-          <h5 class="mt-0">Topic Heading</h5>
-          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-          scelerisque ante sollicitudin. Cras purus odio, vestibulum in
-          vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
-          vulputate fringilla. Donec lacinia congue felis in faucibus.
-          <div class="media mt-3">
-            {/* <img src="/../assets/images/" alt="Generic placeholder image" /> */}
-
-            <div class="media-body">
-              <h5 class="mt-0">Media heading</h5>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-              scelerisque ante sollicitudin. Cras purus odio, vestibulum in
-              vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
-              nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-            </div>
-          </div>
+    <div className="Board text-center  ">
+      <div className="lead text-white">Welcome to the Board</div>
+      <hr />
+      <center>
+        <div className="col-sm-6">
+          <input
+            id="topic"
+            type="text"
+            placeholder="Enter Topic"
+            required="required"
+            onChange={(e) => setTopicState(e.target.value)}
+          ></input>
+          <button type="button" id="create" onClick={(e) => createTopic(e)}>
+            Post
+          </button>
         </div>
-      </div>
+      </center>
+
+      {topicData.map(({ Topic, _id }) => (
+        <TopicPost Topic={Topic} _id={_id} key={_id} />
+      ))}
+
+      {/* {topicData.map(({ Topic, _id }) => (
+        <TopicPost Topic={Topic} _id={_id} key={_id} />
+      ))} */}
     </div>
   );
 };
