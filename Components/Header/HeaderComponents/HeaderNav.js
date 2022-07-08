ae077152a0ac89f2be6bcdcd537f25422ad2b52d
Petyr B #2 <103116958+PetyrB2@users.noreply.github.com>
2022-07-08 10:52:13 +0100

    Updated HeaderNav.js
    Updated the buttons they are smoooth now and larger --- should be positioned better too

--- Components/Header/HeaderComponents/HeaderNav.js
+++ Components/Header/HeaderComponents/HeaderNav.js
@@ -8,29 +8,29 @@ const HeaderNav = () => {
         <nav className="navbar-nav ">
           <ul className="p-2 gt-4">
             <Link to="/">
-              <button type="button" className="btn btn-primary">
+              <button type="button" className="btn btn-primary btn-lg rounded-pill shadow border">
                 Home
               </button>
             </Link>
 
             <Link to="/About">
-              <button type="button" className="btn btn-primary">
+              <button type="button" className="btn btn-primary btn-lg rounded-pill shadow border">
                 About
               </button>
             </Link>
             <Link to="/Searchbar">
-              <button type="button" className="btn btn-primary">
+              <button type="button" className="btn btn-primary btn-lg rounded-pill shadow border">
                 Search
               </button>
             </Link>
 
             <Link to="/Listings">
-              <button type="button" className="btn btn-primary">
+              <button type="button" className="btn btn-primary btn-lg rounded-pill shadow border">
                 Film Listings
               </button>
             </Link>
             <Link to="/NewListings">
-              <button type="button" className="btn btn-primary">
+              <button type="button" className="btn btn-primary btn-lg rounded-pill shadow border">
                 New Films
               </button>
             </Link>
